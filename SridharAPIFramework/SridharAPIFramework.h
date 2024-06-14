c730e87d74c72e63c5e7a1be58a30599cc4fa4b8
Sridhar <sridharkandi01@gmail.com>
2024-06-14 20:34:03 +0530

    Initial Commit

---
+++ SridharAPIFramework/SridharAPIFramework.h
@@ -0,0 +1,18 @@
+//
+//  SridharAPIFramework.h
+//  SridharAPIFramework
+//
+//  Created by Srujana reddy on 14/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SridharAPIFramework.
+FOUNDATION_EXPORT double SridharAPIFrameworkVersionNumber;
+
+//! Project version string for SridharAPIFramework.
+FOUNDATION_EXPORT const unsigned char SridharAPIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SridharAPIFramework/PublicHeader.h>
+
+
